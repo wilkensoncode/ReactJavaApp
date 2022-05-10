46198c32f79d4a47144a62d49fe8ebbb5c2e1bc7
wilkenson <tcbwilkenson@gmail.com>
2022-05-09 23:42:05 -0400

    refactor counter

--- todoreactjava/src/components/counter/Counter.jsx
+++ todoreactjava/src/components/counter/Counter.jsx
@@ -1,38 +1,81 @@
 import "./Counter.css";
 
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class Counter extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      counter: 0,
+    };
+
+    this.increment = this.increment.bind(this);
+  }
+  render() {
+    return (
+      <div className="counter">
+        <CounterButton counter={1} increment={this.increment} />
+        <CounterButton counter={2} increment={this.increment} />
+        <CounterButton counter={5} increment={this.increment} />
+        <span className="count">{this.state.counter}</span>
+      </div>
+    );
+  }
+
+  increment(counter) {
+    this.setState({
+      counter: this.state.counter + counter,
+    });
+  }
+}
+
+class CounterButton extends Component {
   constructor() {
     super();
     this.state = {
       counter: 0,
     };
     this.increment = this.increment.bind(this);
-    this.decrement = this.decrement.bind(this);
+    // this.decrement = this.decrement.bind(this);
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.increment}>+1</button>
-        <span className="count">{this.state.counter}</span>
-        <button onClick={this.decrement}>+1</button>
+        <button onClick={this.increment}>+{this.props.counter}</button>
+        {/* <span counter={this.increment} className="count">
+          {this.state.counter}
+        </span> */}
+        {/* <button onClick={this.decrement}>-1</button> */}
       </div>
     );
   }
 
   increment() {
     this.setState({
-      counter: this.state.counter + 10,
+      counter: this.state.counter + this.props.counter,
     });
+    this.props.increment(this.props.counter);
   }
 
-  decrement() {
-    this.setState({
-      counter: this.state.counter - 5,
-    });
-  }
+  // decrement() {
+  //   {
+  //     if (this.state.counter === 0) return;
+  //     this.setState({
+  //       counter: this.state.counter - 5,
+  //     });
+  //   }
+  // }
 }
+
+CounterButton.defaultProps = {
+  counter: 1,
+};
+
+CounterButton.propTypes = {
+  counter: PropTypes.number,
+};
 
 export default Counter;
